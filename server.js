77c6b9f68c53786647e630cc6510b2ee452321a4
Zach Nicholas <zwnich@gmail.com>
2016-08-21 20:12:35 -0500

    added basic headless implementation

--- server.js
+++ server.js
@@ -2,45 +2,51 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var through2 = require('through2');
-var PATH = 'http://api.openweathermap.org/data/2.5/weather?q=Eugene,US&APPID=977c999ea8a4fa9a2b50236010738a61';
+var cityDB = require('./city.list.us.json');
+var PATH = 'http://api.openweathermap.org/data/2.5/weather';
 
-//var APIKEY; 
 // api call format: //  http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY} 
 // I need to be able to parse the user request's url for the city whose weather they want to compare to SF.
 // Can I test this feature purely in the backend? Right now I'm just jumping straight to the API call.
 // We have a URL -- localhost!! Get the parameter from the url
    function onRequest(request, response) {
-      http.get('http://api.openweathermap.org/data/2.5/weather?q=Eugene,US&APPID=977c999ea8a4fa9a2b50236010738a61',
-      (res) => {
-         console.log(`Got response: ${res.statusCode}`)
          if (request.method === 'GET') {
             
-            var parsed = url.parse(request.url, parseQueryString = true);
-            console.log(parsed.query);
-            var resbody = [];
+            var parsedClientReq = url.parse(request.url, parseQueryString = true);
+            var parsed = url.parse(PATH, parseQueryString = true);
+            // add logic to differentiate between when user asks for direct city name or id
+            parsed.query['q'] = parsedClientReq.query['q'];
+            parsed.query['APPID'] = process.env.APIKEY;
+            console.log('parsed url sent to openweather: ' + url.format(parsed));
+            
+            http.get(url.format(parsed), (res) => {
+               console.log(`Got response: ${res.statusCode}`);
+               var resbody = [];
 
-            res.on('data', function(chunk) {
-               resbody.push(chunk);
-            }).on('end', function() {
-               resbody = Buffer.concat(resbody);
-               //console.log(`Can we print out a raw JSON property? : ${resbody.id}`);// no we can't. It's undefined.
-               var jsn = JSON.parse(resbody);
-               console.log(`Eugene,OR ID: ${jsn.id}`);
-               //console.log(typeof jsn.id); // it's a number! That's why we need to toString() it below
+               res.on('data', function(chunk) {
+                  resbody.push(chunk);
+               }).on('end', function() {
+                  resbody = Buffer.concat(resbody);
+                  //console.log(`Can we print out a raw JSON property? : ${resbody.id}`);// no we can't. It's undefined.
+                  var jsn = JSON.parse(resbody);
+                  console.log(jsn);
+                  console.log(`The weather in ${jsn.name} is ${jsn.weather[0].description}. It\'s currently ${jsn.main['temp']} degrees K.`);
+                  //console.log(typeof jsn.id); // it's a number! That's why we need to toString() it below
 
-               response.writeHead(200, {"Content-Type": "text/plain"});
-               response.end(jsn.id.toString() + '\n');// you can only write back strings or buffers!!!
+                  response.writeHead(200, {"Content-Type": "text/plain"});
+                  response.end(jsn.id.toString() + '\n');// you can only write back strings or buffers!!!
+               }).on('error', (err) => {
+                  console.log(`Got error: ${err}`);
+               });
             });
-         };
-      else 
-         (response.end('Send me a GET'));
-      }).on('error', (err) => {
-         console.log(`Got error: ${err.message}`);
-      });
+        } 
+         else response.end('Send me a GET');
    };
 
 
    var server = http.createServer(onRequest);
    server.listen(8888);
    console.log('Server has started');
-   console.log(url.parse(PATH, parseQueryString = true).query);
+   
+   function compare() {
+   };
