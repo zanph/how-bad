2686f518a91608193034bb42ade7671fbd4a1e72
Zach Nicholas <zwnich@gmail.com>
2016-08-23 13:09:21 -0500

    tidying up

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var through2 = require('through2');
-var cityDB = require('./city.list.us.json');
+//var through2 = require('through2');
+//var cityDB = require('./city.list.us.json');
 var PATH = 'http://api.openweathermap.org/data/2.5/weather';
 
 // api call format: //  http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID={APIKEY} 
-// I need to be able to parse the user request's url for the city whose weather they want to compare to SF.
-// Can I test this feature purely in the backend? Right now I'm just jumping straight to the API call.
-// We have a URL -- localhost!! Get the parameter from the url
    function onRequest(request, response) {
          if (request.method === 'GET') {
             
@@ -34,7 +31,7 @@ var PATH = 'http://api.openweathermap.org/data/2.5/weather';
                   //console.log(typeof jsn.id); // it's a number! That's why we need to toString() it below
 
                   response.writeHead(200, {"Content-Type": "text/plain"});
-                  response.end(jsn.id.toString() + '\n');// you can only write back strings or buffers!!!
+                  response.end(`${jsn.name.toString()}, ${jsn.sys.country.toString()}: ${jsn.weather[0].description.toString()} and ${jsn.main.temp.toString()} degrees K \n`);// you can only write back strings or buffers!!!
                }).on('error', (err) => {
                   console.log(`Got error: ${err}`);
                });
