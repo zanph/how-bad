e3115ede4dde9aae0e5a41f65ff39d7c330024bd
Zach Nicholas <zwnich@gmail.com>
2016-08-25 21:01:59 -0500

    basic input validation almost done. Database tied in

--- server.js
+++ server.js
@@ -9,66 +9,99 @@ var PATH = 'http://api.openweathermap.org/data/2.5/weather';
    function onRequest(request, response) {
          if (request.method === 'GET') {
             
+            // parsedClientReq is the parsed curl request
             var parsedClientReq = url.parse(request.url, parseQueryString = true);
-            var parsed = url.parse(PATH, parseQueryString = true);
-            // add logic to differentiate between when user asks for direct city name or id
-            validateReq(parsed.query);
+            var apiReq = url.parse(PATH, parseQueryString = true);
+            // validateReq modifies the api query object as a side effect. This is by design.
+            if (!validateReq(parsedClientReq.query, apiReq.query)) {
+               console.error(`Error: invalid query ${parsedClientReq}`);
+               process.exit;
+            }
 
-            parsed.query['q'] = parsedClientReq.query['q'];
-            parsed.query['APPID'] = process.env.APIKEY;
-            console.log('parsed url sent to openweather: ' + url.format(parsed));
-            
-            http.get(url.format(parsed), (res) => {
-               console.log(`Got response: ${res.statusCode}`);
-               var resbody = [];
+            else {
 
-               res.on('data', function(chunk) {
-                  resbody.push(chunk);
-               }).on('end', function() {
-                  resbody = Buffer.concat(resbody);
-                  //console.log(`Can we print out a raw JSON property? : ${resbody.id}`);// no we can't. It's undefined.
-                  var jsn = JSON.parse(resbody);
-                  console.log(jsn);
-                  console.log(`The weather in ${jsn.name} is ${jsn.weather[0].description}. It\'s 
-                        currently ${jsn.main['temp']} degrees K.`);
-                     //console.log(typeof jsn.id); // it's a number! That's why we need to toString() it below
+               console.log('parsed url sent to openweather: ' + url.format(apiReq));
+               // I need to refactor the streaming of the get request to use through2.
+               // Should be significantly fewer lines
+               http.get(url.format(apiReq), (res) => {
+                  console.log(`Got response: ${res.statusCode}`);
+                  var resbody = [];
 
-                  response.writeHead(200, {"Content-Type": "text/plain"})
-                  // you can only write back strings or buffers!!!;
-                  response.end(`${jsn.name.toString()}, ${jsn.sys.country.toString()}:
-                         ${jsn.weather[0].description.toString()} and ${jsn.main.temp.toString()} degrees K`);
-                  }).on('error', (err) => {
-                  console.log(`Got error: ${err}`);
+                  res.on('data', function(chunk) {
+                     resbody.push(chunk);
+                  }).on('end', function() {
+                     resbody = Buffer.concat(resbody);
+                     //console.log(`Can we print out a raw JSON property? : ${resbody.id}`);// no we can't. It's undefined.
+                     var jsn = JSON.parse(resbody);
+                     console.log(jsn);
+                     console.log(`The weather in ${jsn.name} is ${jsn.weather[0].description}. It\'s 
+                           currently ${jsn.main['temp']} degrees K.`);
+                        //console.log(typeof jsn.id); // it's a number! That's why we need to toString() it below
+
+                     response.writeHead(200, {"Content-Type": "text/plain"})
+                     // you can only write back strings or buffers!!!;
+                     response.end(`${jsn.name.toString()}, ${jsn.sys.country.toString()}:
+                            ${jsn.weather[0].description.toString()} and ${jsn.main.temp.toString()} degrees K`);
+                     }).on('error', (err) => {
+                     console.log(`Got error: ${err}`);
+                  });
                });
-            });
+            }
         } 
          else response.end('Send me a GET');
    };
 
-   function validateReq(query) {
+   function validateReq(clientReq, apiQuery) {
       // creates appropriate query object for api call via zipcode or city name
-      // and checks it against the world city database
-      // May be better to search the DB for the city, and then just call the API
-      // using it's unique ID. 
+      // and checks it against the world city database. If city name & country
+      // are found, prepares to call API with unique city ID.
+      // Returns 1 if we have a valid query, 0 otherwise
       zipRe = /\s*[^A-Za-z][1-9]\d{3,4}$/;
-      if (zipRe.match(query)) { // match, call API by zipcode
-         zip = zipRe.match(query);
-         query['zip'] = zip + ',US';
+      if (clientReq['q'].match(zipRe)) {     //  call API by zipcode
+         apiQuery['zip'] = clientReq['q'].match(zipRe) + ',US';
+         apiQuery['APPID'] = process.env.APIKEY;
+          
+         return 1;
       }
       else { // assume client is searching by city
-        separated = query.split(',');
+        separated = clientReq['q'].split(',');
         city = separated[0].trim();
         country = separated[1].trim();
-        query['q'] = city + ',' + country;
+        id = findID(cityDB, city, country); 
+        
+        if (id !== 0) {
+           apiQuery['id'] = id;
+           apiQuery['APPID'] = process.env.APIKEY;
+        
+           return 1;
+        }
+        else {
+           return 0;  // 0 indicates clientReq not found in db
+        }
       }
+   };
 
+   function findID(cityDB, city, country) {
+      // search cityDB for queried city and country.
+      // if found, return matching db object, else, return undefined
+      // for now, return first match
+      // must address possibility of multiple matches
+      for (var i = 0; i < cityDB.length; i++) {
+         if (cityDB[i].name === city && cityDB[i].country === country) {
+            console.log(`I found id ${cityDB[i]._id} matching city ${city},${country}`);
+            return cityDB[i]._id;
+         }
+         else continue;
+      }
+         return 0; // sentinel value -- city/country not found
+   };
 
-   }
 
 
    var server = http.createServer(onRequest);
    server.listen(8888);
    console.log('Server has started');
-   
-   function compare() {
-   };
+
+   // actual city comparison operation
+   //function compare() {
+   //};
