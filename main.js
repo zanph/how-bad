a4333701b0d72780e33304856c656e4e67e0e01d
Preston Hansen <prestonhansen01@gmail.com>
2016-09-03 22:47:59 -0500

    Add snippets to main.js for frontend ideas.

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-$(document).ready(function() {
+$(document).ready( function () {
     
     $('#random-button').click( function () {
-        //api call for weather, hardcoded for now
+        //pick 2 random ids from the database
+        //which will be of the form: //todo
+        //and compare them.
     });
 
-
+    //using this to see if jquery.serialize()
+    //is useful/good enough for our purposes..
+    $('form').on( 'submit', function( event ) {
+        event.preventDefault();
+        console.log( $( this ).serialize() );
+    });
+    
+    $('#search-button').click( function () {
+        // $.ajax({
+        //     url: 'some-url',
+        //     type: 'post',
+        //     dataType: 'json',
+        //     data: $('form#myForm').serialize(),
+        //     success: function(data) {
+        //         //modify the DOM
+        //     }
+        // });
+    });
 
 });
