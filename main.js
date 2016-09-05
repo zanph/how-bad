0c3d564ad9aa5623f7973ef7399b67bcd1655133
Preston Hansen <prestonhansen01@gmail.com>
2016-09-05 09:45:10 -0500

    Add empty-form error states and UI using jquery.

--- main.js
+++ main.js
@@ -9,10 +9,35 @@ $(document).ready( function () {
     //using this to see if jquery.serialize()
     //is useful/good enough for our purposes..
     //looks like it is!
-    $('form').on( 'submit', function( event ) {
+    $('form').on( 'submit', function (event) {
         event.preventDefault();
+        
+        //make sure the form is not empty.
+        //if it is, utilize bootstrap form error states.
+        $(':text').each( function () {
+            if($(this).val() === "") {
+                formIsEmpty = true;
+                //if we don't already have an error message, set the
+                //error state and add the help text
+                if(!$(this).parent().hasClass('has-error')) {
+                    
+                    $(this).parent().addClass('has-error');
+                    $(this).parent().before(
+                        '<p class="text-danger">\
+                        Whoops! Enter a location.</p>'
+                    );
+                }
+            }
+            else {
+               $(this).parent().removeClass('has-error');
+               $(this).parent().prev('p.text-danger').remove();
+            }
+        });
+
         console.log('captured form submit!');
         console.log( $(this).serialize() );
+
+        
         // $.ajax({
         //     url: 'some-url',
         //     type: 'get',
